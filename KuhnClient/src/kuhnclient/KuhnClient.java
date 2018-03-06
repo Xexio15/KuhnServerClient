1885c50433683abe43c5b4fea6d1bdc008adca3b
Sergio <>
2018-03-06 19:00:30 +0100

    Base del Client

--- KuhnClient/src/kuhnclient/KuhnClient.java
+++ KuhnClient/src/kuhnclient/KuhnClient.java
@@ -5,6 +5,9 @@
  */
 package kuhnclient;
 
+import java.io.*;
+import java.rmi.*;
+
 /**
  *
  * @author xexio
@@ -15,7 +18,32 @@ public class KuhnClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            int RMIPort;         
+            String hostName;
+            String portNum;
+            InputStreamReader is = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(is);
+            
+            System.out.println("Enter the RMIRegistry host namer:");
+            hostName = br.readLine();
+            //Demanem el port a connectarse
+            System.out.println("Enter the RMIRegistry port number:");
+            portNum = br.readLine();
+            RMIPort = Integer.parseInt(portNum);
+            
+            // Look up the remote object and cast its reference 
+            // to the remote interface class -- replace "localhost"
+            // with the appropriate host name of the remote object.
+            String registryURL = "rmi://localhost:" + portNum + "/some";  
+            KuhnInterface h = (KuhnInterface)Naming.lookup(registryURL);
+            // invoke the remote method(s)
+            //String message = h.method1();
+            //System.out.println(message);
+            // method2 can be invoked similarly
+        } 
+        catch (Exception ex) {
+            ex.printStackTrace( );
+        } 
     }
-    
 }
